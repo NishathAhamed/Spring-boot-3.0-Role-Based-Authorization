f7b8888ddb1d29818d045f7139a865c4c7ce44ad
Nishath Ahamed <117764163+NishathAhamed@users.noreply.github.com>
2023-07-03 20:32:20 +0530

    created ApplicationConfig class

--- src/main/java/com/example/RoleBasedAuthorization/repository/UserRepository.java
+++ src/main/java/com/example/RoleBasedAuthorization/repository/UserRepository.java
@@ -2,8 +2,14 @@ package com.example.RoleBasedAuthorization.repository;
 
 import com.example.RoleBasedAuthorization.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+
+    Optional<User> findByUserName(String username);
 }
